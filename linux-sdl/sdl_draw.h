6ed457e6d16ff8118da6065f506fc49778972b6a
artanejp <artanejp@a0c0af59-af12-4589-88dc-6bbe4490f711>
2010-02-01 17:40:26 +0000

    [UI][GENERAL] Separate GTK+/GDK depended part from sdl_main.c to gtk_main.c. [GENERAL] Separate GTK part (MORE..) [UI] rename sdl_prop.c -> gtk_prop.c .
    git-svn-id: https://xm7-sdl.unfuddle.com/svn/xm7-sdl_xm7-sdl-linux@28 a0c0af59-af12-4589-88dc-6bbe4490f711

--- linux-sdl/sdl_draw.h
+++ linux-sdl/sdl_draw.h
@@ -1,21 +1,24 @@
 /*
- *  FM-7 EMULATOR "XM7"  Copyright (C) 1999-2003
- * ＰＩ．(ytanaka@ipc-tokai.or.jp) Copyright (C) 2001-2003 Ryu
- * Takegami Copyright (C) 2004 GIMONS  [ XWIN 表示 ] 
+ *  FM-7 EMULATOR "XM7"
+ *  Copyright (C) 1999-2003 ＰＩ．(ytanaka@ipc-tokai.or.jp)
+ *  Copyright (C) 2001-2010 Ryu Takegami
+ *  Copyright (C) 2004 GIMONS
+ *  Copyright (C) 2010 K.Ohta
+ *  [ SDL 表示 ] 
  */  
     
-#ifdef _XWIN
+
     
-#ifndef _xw_draw_h_
-#define _xw_draw_h_
+#ifndef _sdl_draw_h_
+#define _sdl_draw_h_
     
 #ifdef __cplusplus
 extern          "C" {
     
-#endif				/*  */
+#endif				/*  */
     
 	/*
-	 *  主要エントリ 
+	 *  主要エントリ 
 	 */ 
     void            InitDraw(void);
                    
@@ -37,32 +40,42 @@ extern          "C" {
 	/*
 	 * 描画 
 	 */            
-     
 	
-	
-	
-	
-	          gint OnPaint(GtkWidget * widget, GdkEventExpose * event);
-                   
+#ifdef USE_GTK	
+        gint OnPaint(GtkWidget * widget, GdkEventExpose * event);
+#else
+        int  OnPaint(void);
+#endif                   
 	/*
 	 * 再描画 
 	 */            
-    void            OnFullScreen(void);
-                   void OnWindowedScreen(void);
-                
+        void    OnFullScreen(void);
+        void    OnWindowedScreen(void);
+                
 	/*
-	 *  主要ワーク 
+	 *  主要ワーク 
 	 */            
-    extern BOOL     bFullScan;
-                   extern WORD nDrawWidth;
-                   extern WORD nDrawHeight;
+        extern WORD  nDrawWidth;
+        extern WORD  nDrawHeight;
+        extern DWORD    rgbTTLGDI[16];	/* デジタルパレット */
+        extern DWORD    rgbAnalogGDI[4096];	/* アナログパレット */
+        extern BYTE     GDIDrawFlag[4000];	/* 8x8 再描画領域フラグ */
+        extern BOOL     bFullScan;	/* フルスキャン(Window) */
+        extern BOOL     bDirectDraw;	/* 直接書き込みフラグ */
+        extern SDL_Surface *realDrawArea;	/* 実際に書き込むSurface(DirectDrawやOpenGLを考慮する)  */
                    
+        extern WORD     nDrawTop;	/* 描画範囲上 */
+        extern WORD     nDrawBottom;	/* 描画範囲下 */
+        extern WORD     nDrawLeft;	/* 描画範囲左 */
+        extern WORD     nDrawRight;	/* 描画範囲右 */
+        extern BOOL     bPaletFlag;	/* パレット変更フラグ */
+        extern BOOL     bClearFlag;	/* クリアフラグ */
 	/*
 	 * フルスキャン(Window) 
 	 */            
 #ifdef __cplusplus
 }              
-#endif				/*  */
+#endif				/*  */
                
-#endif	/* _xw_draw_h_ */
-#endif	/* _XWIN */
+#endif	/* _sdl_draw_h_ */
+
