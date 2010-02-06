b4fdb12b4fc4085ac7e143e18986c3bb5b2887c7
artanejp <artanejp@a0c0af59-af12-4589-88dc-6bbe4490f711>
2010-02-06 19:16:14 +0000

    [UI][KBD] Add redefun.keyboard(1st).
    git-svn-id: https://xm7-sdl.unfuddle.com/svn/xm7-sdl_xm7-sdl-linux@34 a0c0af59-af12-4589-88dc-6bbe4490f711

---
+++ linux-sdl/gtk_propkeyboard.h
@@ -0,0 +1,28 @@
+/*
+ *  FM-7 EMULATOR "XM7"
+ *  Copyright (C) 1999-2003 ＰＩ．(ytanaka@ipc-tokai.or.jp)
+ *  Copyright (C) 2001-2010 Ryu Takegami
+ *  Copyright (C) 2004 GIMONS
+ *  Copyright (C) 2010 K.Ohta  
+ * [ XWIN/SDLキーボード設定プログラム - GTK+パート] 
+ */ 
+#ifndef _gtk_propkeyboard_h_
+ 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+        extern BOOL     SnoopedOnKeyPressedCallback(SDL_Event *event);
+        //extern void     SnoopKeyEvent(BOOL flag);
+        extern void     StartGetKeycodeForProp(Uint16 keysym, BYTE code);
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif /* _gtk_propkeyboard_h_ */
+
