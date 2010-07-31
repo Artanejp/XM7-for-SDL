2a369cbdac0495a4e940e62a92d348565cac2ca8
artanejp <artanejp@a0c0af59-af12-4589-88dc-6bbe4490f711>
2010-07-31 11:30:16 +0000

    [BUILD] Eclipse IDEの排除／再設定
    git-svn-id: https://xm7-sdl.unfuddle.com/svn/xm7-sdl_xm7-sdl-linux@60 a0c0af59-af12-4589-88dc-6bbe4490f711

--- linux-sdl/build-scripts/gcc-cross.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-IA32BOOTSTRAPED=/emul/ia32-bootstrap
-ADDCFLAGS="-I/emul/ia32-bootstrap/usr/include -I/emul/ia32-bootstrap/usr/local/include -I/emul/ia32-bootstrap/usr/X11R6/include -I. -D_REENTRANT -pthread -I/emul/ia32-bootstrap/usr/include/gtk-2.0                 -I/emul/ia32-bootstrap/usr/lib/gtk-2.0/include  -I/emul/ia32-bootstrap/usr/include/atk-1.0 -I/emul/ia32-bootstrap/usr/include/cairo -I/emul/ia32-bootstrap/usr/include/pango-1.0 -I/emul/ia32-bootstrap/usr/include/pixman-1 -I/emul/ia32-bootstrap/usr/include/freetype2 -I/emul/ia32-bootstrap/usr/include/directfb -I/emul/ia32-bootstrap/usr/include/libpng12 -I/emul/ia32-bootstrap/usr/include/glib-2.0 -I/emul/ia32-bootstrap/usr/lib/glib-2.0/include -I/emul/ia32-bootstrap/usr/include/libglade-2.0/ -pthread -I/usr/include/glib-2.0 -I/emul/ia32-bootstrap/usr/lib/glib-2.0/include"
-
-gcc -m32 $ADDCFLAGS $@
