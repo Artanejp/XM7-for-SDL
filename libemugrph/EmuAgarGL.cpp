d8f1bd241bb7c248ae0a7674405ec17b95f1558b
artanejp <artanejp@a0c0af59-af12-4589-88dc-6bbe4490f711>
2010-11-09 15:49:19 +0000

    [Draw] Add SetWidget();
    git-svn-id: https://xm7-sdl.unfuddle.com/svn/xm7-sdl_xm7-sdl-linux@162 a0c0af59-af12-4589-88dc-6bbe4490f711

--- libemugrph/EmuAgarGL.cpp
+++ libemugrph/EmuAgarGL.cpp
@@ -9,6 +9,7 @@
 
 #include "EmuAgarGL.h"
 
+
 EmuAgarGL::EmuAgarGL() {
 	// TODO Auto-generated constructor stub
 	minX = 0.0f;
@@ -18,11 +19,13 @@ EmuAgarGL::EmuAgarGL() {
 	InitVideo = TRUE;
     ScanLine = FALSE;
     ScanLineWidth = 4.0f;
-
+    UseTexture = FALSE;
+    video = NULL;
 }
 
 EmuAgarGL::~EmuAgarGL() {
 	// TODO Auto-generated destructor stub
+	if(video != NULL) AG_SurfaceFree(video);
 }
 
 void EmuAgarGL::InitUI(char *name, Uint Flags)
@@ -33,6 +36,17 @@ void EmuAgarGL::InitUI(char *name, Uint Flags)
 void EmuAgarGL::InitUI(char *name)
 {
 	InitUI(name, AG_VERBOSE | AG_CREATE_DATADIR | AG_NO_CFG_AUTOLOAD);
+}
+
+void EmuAgarGL::SetDrawArea(AG_Window *p, int x, int y, int w, int h)
+{
+	if(drawarea == NULL) {
+		drawarea = AG_BoxNew(p, AG_BOX_HORIZ,0);
+	}
+	if(drawarea != NULL){
+		AG_WidgetSetPosition(&(drawarea->wid), x, y);
+		AG_WidgetSetSize(&(drawarea->wid), w, h);
+	}
 }
 
 void EmuAgarGL::InitGL(int w, int h)
@@ -106,3 +120,65 @@ static inline void SetByte(Uint8 *addr, Uint32 *c)
 	putdot(&addr[28], c[0]);
 }
 
+void EmuAgarGL::PutVram(AG_Surface *p, int x, int y, int w, int h, Uint32 mpage)
+{
+	int xx, yy;
+	int hh, ww;
+	int addr;
+	int size;
+	int ofset;
+	int i;
+	Uint32 c[8];
+	Uint8 *bitmap;
+	Uint8 *disp;
+	AG_Driver *drv;
+
+
+	if(!InitVideo) return;
+	if(drawarea == NULL) return;
+	if(putword == NULL) return;
+	// Test
+	if(agDriverOps == NULL) return;
+	drv = AG_DriverOpen(agDriverOps);
+	if(drv == NULL) return;
+#if 0
+	surface = SDL_GetVideoSurface();
+	printf("Video w: %d h:%d FLAGS:%02x\n", surface->w, surface->h, surface->flags);
+#endif
+	size = vramwidth * vramheight * 8 * 4;
+	glClearColor(0, 0, 0, 0);
+	ww = w >>3;
+	hh = h + y;
+
+	if(video == NULL) {
+		AG_DriverClose(drv);
+		return;
+	}
+	bitmap = (Uint8 *)video->pixels;
+	ofset = 0;
+	for(yy = y; yy < hh; yy++) {
+		for(xx = 0; xx < ww; xx++) {
+			addr = yy  * vramwidth + xx + x;
+			ofset = yy * vramwidth * 32 + xx * 32;
+			getvram(addr, c, mpage);
+			disp = &bitmap[ofset];
+			putword((Uint32 *)disp, 32, c);
+			ofset+=32;
+			addr++;
+			}
+	}
+#if 0
+	printf("Transfer: %08x bytes \n", ofset);
+#endif
+	agDriverOps->blitSurfaceGL(drv, &(drawarea->wid), video, 1.0, 1.0);
+
+//   if(ScanLine) {
+//	   DrawScanLine();
+//   }
+
+#if 0
+	printf("Draw: %08x bytes TID=%08x\n", ofset, textureid);
+#endif
+	AG_DriverClose(drv);
+
+}
--- libemugrph/EmuAgarGL.h
+++ libemugrph/EmuAgarGL.h
@@ -9,9 +9,11 @@
 #define EMUAGARGL_H_
 
 #include "EmuGLUtils.h"
-#include <agar/core/types.h>
 #include <agar/core.h>
+#include <agar/core/types.h>
 #include <agar/gui.h>
+#include <agar/gui/opengl.h>
+
 
 #ifndef Uint
 #define Uint unsigned int
@@ -24,20 +26,21 @@ public:
 	void InitUI(char *name);
 	void InitUI(char *name, Uint Flags);
 	void InitGL(int w, int h);
-	void PutVram(SDL_Surface *p, int x, int y, int w, int h, Uint32 mpage);
-	void SetViewPort(int x, int y, int w, int h);
-	void SetViewPort(void);
+	void SetDrawArea(AG_Window *p, int x, int y, int w, int h);
+	void PutVram(AG_Surface *p, int x, int y, int w, int h, Uint32 mpage);
 	void Flip(void);
     void SetScanLine(BOOL flag);
     void CalcPalette(Uint32 src, Uint8 r, Uint8 g, Uint8 b, Uint8 a);
     void SetPaletteTable(Uint32 *p);
-
 protected:
     AG_Surface *video;
-    AG_PixelFormat format;
     Uint32 *palette;
     BOOL   InitVideo;
+    Uint textureid;
+    AG_Box *drawarea;
+    AG_PixelFormat format;
 private:
+    BOOL  UseTexture;
 };
 
 #endif /* EMUAGARGL_H_ */
