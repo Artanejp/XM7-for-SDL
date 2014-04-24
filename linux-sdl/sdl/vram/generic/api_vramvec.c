4c5d4fd50fef756cdba0c0d8e076dd939e66a2c5
K.Ohta <whatisthis.sowhat@gmail.com>
2014-04-25 06:57:33 +0900

    [Draw] Use __builtin_prefetch for conversion-table, a bit of faster(maybe..)

--- linux-sdl/sdl/vram/generic/api_vramvec.c
+++ linux-sdl/sdl/vram/generic/api_vramvec.c
@@ -86,7 +86,22 @@ void initvramtbl_4096_vec(void)
         r.v <<= 1;
         aPlanes[G3 + i] = r.v;
 //        r.v <<= 1;
+      __builtin_prefetch(&aPlanes[B0 + i], 0, 3);
+      __builtin_prefetch(&aPlanes[B1 + i], 0, 3);
+      __builtin_prefetch(&aPlanes[B2 + i], 0, 3);
+      __builtin_prefetch(&aPlanes[B3 + i], 0, 3);
+
+      __builtin_prefetch(&aPlanes[R0 + i], 0, 3);
+      __builtin_prefetch(&aPlanes[R1 + i], 0, 3);
+      __builtin_prefetch(&aPlanes[R2 + i], 0, 3);
+      __builtin_prefetch(&aPlanes[R3 + i], 0, 3);
+
+      __builtin_prefetch(&aPlanes[G0 + i], 0, 3);
+      __builtin_prefetch(&aPlanes[G1 + i], 0, 3);
+      __builtin_prefetch(&aPlanes[G2 + i], 0, 3);
+      __builtin_prefetch(&aPlanes[G3 + i], 0, 3);
     }
+	
 }
 
 void detachvramtbl_8_vec(void)
