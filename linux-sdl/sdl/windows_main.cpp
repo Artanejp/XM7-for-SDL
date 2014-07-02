3da93990aa7d48dc444fe87f9bf4910a60c0c04e
K.Ohta <whatisthis.sowhat@gmail.com>
2014-07-02 16:19:33 +0900

    [General][LOG] Move initialize messages to main() in window_main.cpp, real startup routine.

--- linux-sdl/sdl/windows_main.cpp
+++ linux-sdl/sdl/windows_main.cpp
@@ -39,6 +39,7 @@
 #include "agar_cfg.h"
 #include "agar_draw.h"
 #include "agar_gldraw.h"
+#include "agar_logger.h"
 
 #include "sdl_inifile.h"
 #include "sdl_cpuid.h"
@@ -51,20 +52,29 @@ extern int MainLoop(int argc, char *argv[]);
 
 extern "C" {
 extern struct  XM7_CPUID *pCpuID;           /* CPUフラグ */
+BOOL            bLogSTDOUT = FALSE;
+BOOL            bLogSYSLOG = FALSE;
+
+   
 int main(int argc, char *argv[])
 {
    int rgb_size[3];
    int flags;
+   char simdstr[512];
+   char archstr[32];
+
 /*
  * 実行モジュールのディレクトリを取得
  */
-        char    *p;
+   char    *p;
+
+   int simdid = 0;
        /*
 	* Check CPUID
 	*/
        pCpuID = initCpuID();
        p = SDL_getenv("HOME");
-        if(p == NULL) {
+       if(p == NULL) {
 #ifdef _WINDOWS
                 perror("Warning : Can't get HOME directory...Making .\\xm7\\ .");
                strcpy(ModuleDir, ".\\xm7\\");
@@ -80,19 +90,126 @@ int main(int argc, char *argv[])
                 strcat(ModuleDir, "/.xm7/");
 #endif
         }
+   
+        AG_InitCore("xm7", AG_VERBOSE | AG_CREATE_DATADIR);
+        AG_ConfigLoad();
 
-       printf("Moduledir = %s home = %s\n", ModuleDir, p); // Debug
-        if(opendir(ModuleDir) == NULL) {
-#ifdef _WINDOWS
-		mkdir(ModuleDir);
-#else
-        mkdir(ModuleDir, 0777);
+        if(AG_GetVariable(agConfig, "logger.syslog", NULL) == NULL) { 
+	   AG_SetInt(agConfig, "logger.syslog", FALSE);
+	}
+        if(AG_GetVariable(agConfig, "logger.stdout", NULL) == NULL) { 
+	   AG_SetInt(agConfig, "logger.stdout", TRUE);
+	}
+        bLogSYSLOG = (BOOL)AG_GetInt(agConfig, "logger.syslog");
+        bLogSTDOUT = (BOOL)AG_GetInt(agConfig, "logger.stdout");
+        XM7_OpenLog(bLogSYSLOG, bLogSTDOUT); // Write to syslog, console
+
+   strncpy(archstr, "Generic", 30);
+#if defined(__x86_64__)
+   strncpy(archstr, "amd64", 30);
 #endif
+#if defined(__i386__)
+   strncpy(archstr, "ia32", 30);
+#endif
+        printf("XM7/SDL %s%s %s %s\n", VERSION, LEVEL, LOCALVER, DATE);
+        printf("(C) Ryu Takegami / SDL Version K.Ohta <whatisthis.sowhat@gmail.com>\n");
+        printf("Architecture: %s\n", archstr);
+        printf(" -? is print help(s).\n");
+   
+        /* Print SIMD features */ 
+        simdstr[0] = '\0';
+#if defined(__x86_64__) || defined(__i386__)
+        if(pCpuID != NULL) {
+	   
+	   if(pCpuID->use_mmx) {
+	      strcat(simdstr, " MMX");
+	      simdid |= 0xffffffff;
+	   }
+	   if(pCpuID->use_mmxext) {
+	      strcat(simdstr, " MMXEXT");
+	      simdid |= 0xffffffff;
+	   }
+	   if(pCpuID->use_sse) {
+	      strcat(simdstr, " SSE");
+	      simdid |= 0xffffffff;
+	   }
+	   if(pCpuID->use_sse2) {
+	      strcat(simdstr, " SSE2");
+	      simdid |= 0xffffffff;
+	   }
+	   if(pCpuID->use_sse3) {
+	      strcat(simdstr, " SSE3");
+	      simdid |= 0xffffffff;
+	   }
+	   if(pCpuID->use_ssse3) {
+	      strcat(simdstr, " SSSE3");
+	      simdid |= 0xffffffff;
+	   }
+	   if(pCpuID->use_sse41) {
+	      strcat(simdstr, " SSE4.1");
+	      simdid |= 0xffffffff;
+	   }
+	   if(pCpuID->use_sse42) {
+	      strcat(simdstr, " SSE4.2");
+	      simdid |= 0xffffffff;
+	   }
+	   if(pCpuID->use_sse4a) {
+	      strcat(simdstr, " SSE4a");
+	      simdid |= 0xffffffff;
+	   }
+	   if(pCpuID->use_abm) {
+	      strcat(simdstr, " ABM");
+	      simdid |= 0xffffffff;
+	   }
+	   if(pCpuID->use_avx) {
+	      strcat(simdstr, " AVX");
+	      simdid |= 0xffffffff;
+	   }
+	   if(pCpuID->use_3dnow) {
+	      strcat(simdstr, " 3DNOW");
+	      simdid |= 0xffffffff;
+	   }
+	   if(pCpuID->use_3dnowp) {
+	      strcat(simdstr, " 3DNOWP");
+	      simdid |= 0xffffffff;
+	   }
+	   if(simdid == 0) strcpy(simdstr, "NONE");
+	} else {
+	   strcpy(simdstr, "NONE");
+	}
+#else // Not ia32 or amd64
+	   strcpy(simdstr, "NONE");
+#endif
+   printf("Supported SIMD: %s\n", simdstr);
+
+   
+   XM7_DebugLog(XM7_LOG_INFO, "Start XM7 %s%s %s %s", VERSION, LEVEL, LOCALVER, DATE);
+   XM7_DebugLog(XM7_LOG_INFO, "(C) Ryu Takegami / SDL Version K.Ohta");
+   XM7_DebugLog(XM7_LOG_INFO, "Architecture: %s", archstr);
+
+   XM7_DebugLog(XM7_LOG_DEBUG, "Start XM7 %s%s %s %s", VERSION, LEVEL, LOCALVER, DATE);
+   XM7_DebugLog(XM7_LOG_DEBUG, "(C) Ryu Takegami / SDL Version K.Ohta");
+   XM7_DebugLog(XM7_LOG_DEBUG, "Architecture: %s", archstr);
+   XM7_DebugLog(XM7_LOG_DEBUG, "Supported SIMD: %s", simdstr);
+   XM7_DebugLog(XM7_LOG_DEBUG, " -? is print help(s).");
+   XM7_DebugLog(XM7_LOG_DEBUG, "Moduledir = %s home = %s", ModuleDir, p); // Debug
+
+   
+   if(opendir(ModuleDir) == NULL) {
+#ifdef _WINDOWS
+	   mkdir(ModuleDir);
+#else
+	   mkdir(ModuleDir, 0777);
+#endif
+	   XM7_DebugLog(XM7_LOG_DEBUG, "Created directory: %s", ModuleDir);
         }
         /* Gettext */
         setlocale(LC_ALL, "");
         bindtextdomain("messages", RSSDIR);
         textdomain("messages");
+        XM7_DebugLog(XM7_LOG_DEBUG, "I18N via gettext initialized.");
+        XM7_DebugLog(XM7_LOG_DEBUG, "I18N resource dir: %s", RSSDIR);
+        
 
         //SDL_Init(SDL_INIT_EVERYTHING | SDL_INIT_TIMER);
 
