9ede93eb64b72fa36d949a1010a464907e5b5703
K.Ohta <whatisthis.sowhat@gmail.com>
2014-08-14 02:14:05 +0900

    [Build][CMAKE] Add FindIconv.cmake from http://marc.info/?l=cmake&m=134620423520382&w=2 . [Build][CMAKE] Use add_definitions instead of setting complie DEFINES.

--- linux-sdl/cmake/config_xm7.cmake
+++ linux-sdl/cmake/config_xm7.cmake
@@ -12,21 +12,16 @@ set(LOCAL_LIBS
 		     xm7_soundbuffer-generic
 		     )
 
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -DUSE_AGAR")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -DUSE_AGAR")
+add_definitions(-DUSE_AGAR)
 
 # Build Flags
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -DXM7_VER=${XM7_VERSION}")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -DXM7_VER=${XM7_VERSION}")
+add_definitions(-DXM7_VER=${XM7_VERSION})
 
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -DFDDSND")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -DFDDSND")
+add_definitions(-DFDDSND)
 
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -DMOUSE")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -DMOUSE")
+add_definitions(-DMOUSE)
 
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -DMR2 -D_XWIN -DNDEBUG -D_M_IX86")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -DMR2 -D_XWIN -DNDEBUG -D_M_IX86")
+add_definitions(-DMR2 -D_XWIN -DNDEBUG -D_M_IX86)
 
 
 find_package(OpenGL)
@@ -36,8 +31,7 @@ if(USE_OPENCL)
    find_package(OpenCL)
    if(OPENCL_FOUND)
     include_directories(${OPENCL_INCLUDE_DIRS})
-    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -D_USE_OPENCL -DUSE_OPENCL")
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -D_USE_OPENCL -DUSE_OPENCL")
+    add_definitions(-D_USE_OPENCL -DUSE_OPENCL)
     set(OPENCL_LIBRARY ${OPENCL_LIBRARIES})
     set(USE_OPENGL ON)
    endif()
@@ -47,8 +41,7 @@ endif()
 if(USE_OPENGL)
  if(OPENGL_FOUND)
    include_directories(${OPENGL_INCLUDE_PATH})
-   set(CMAKE_C_FLAGS  "${CMAKE_C_FLAGS} -D_USE_OPENGL -DUSE_OPENGL")
-   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -D_USE_OPENGL -DUSE_OPENGL")
+   add_definitions(-D_USE_OPENGL -DUSE_OPENGL)
  else()
    set(USE_OPENGL OFF)
    set(USE_OPENCL OFF)
