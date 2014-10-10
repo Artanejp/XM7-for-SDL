309ccab5ae307070502f922504418f1424dfd2f9
K.Ohta <whatisthis.sowhat@gmail.com>
2014-10-11 02:39:51 +0900

    [UI][Agar][HELP] Add index list.But, still buggy.

--- linux-sdl/ui-agar/agar_menu_help.cpp
+++ linux-sdl/ui-agar/agar_menu_help.cpp
@@ -34,6 +34,11 @@ extern "C" {
 /*
  */
 
+struct index_list{
+  int index;
+  char indexname[512];
+};
+
 static SDL_RWops *OpenInfoByLocale(void)
 {
   char loc[4];
@@ -124,14 +129,128 @@ static Uint8 *ReadInfoFile(SDL_RWops *fp, int *size)
 
 static void OnCloseHelpViewer(AG_Event *event)
 {
-  AG_Button *self = AG_SELF();
-  Uint8 *rawdata = AG_PTR(1);
+  AG_Button           *self = AG_SELF();
+  Uint8               *rawdata = AG_PTR(1);
+  struct index_list   *indexs = AG_PTR(2);
   
   if(rawdata != NULL) free(rawdata);
+  if(indexs != NULL)  free(indexs);
   if(self != NULL) {
     AG_WindowHide(self->wid.window);
     AG_ObjectDetach(self);
   }
+}
+
+static void OnClickNode(AG_Event *event)
+{
+  AG_Widget *self = AG_SELF();
+  AG_Tlist *tbl = AG_PTR(1);
+  AG_Textbox *textbox = AG_PTR(2);
+  struct index_list *index;
+  
+  if(self == NULL) return;
+  if(tbl == NULL) return;
+  index = AG_TlistSelectedItemPtr(tbl);
+  if((index != NULL) && (textbox != NULL)){
+    AG_TextboxSetCursorPos(textbox, index->index);
+  }
+}
+
+
+static int GetIndexLines(char *begin, char *end, struct index_list **indexs)
+{
+  int i = 0;
+  char *p = begin;
+  char *OneLineHead;
+  char *nodehead, *nodename, *nodepos;
+  struct index_list *index1, *index2;
+
+  index1 = malloc(sizeof(struct index_list));
+  
+  do {
+    index1[i].index = 0;
+    index1[i].indexname[0] = '\0';
+    p = strstr(p, "\nNode: ");
+    if(p >= end) break;
+    if(p == NULL) break;
+    
+    OneLineHead = p;
+    nodehead = strtok_r(OneLineHead, "\x7f\x20\n", &OneLineHead); 
+    nodename = strtok_r(OneLineHead, "\x7f\x20\n", &OneLineHead); 
+    nodepos  = strtok_r(OneLineHead, "\x7f\x20\n", &OneLineHead);
+    p = OneLineHead; 
+    if((strcmp(nodehead, "Node:") != 0) || (nodename == NULL) || (nodepos == NULL)) continue;
+    index1[i].index = strtol(nodepos, &nodepos, 10);
+    strncpy(index1[i].indexname, nodename, 511);
+    index2 = realloc(index1, (i + 2) * sizeof(struct index_list));
+    if(index2 == NULL) {
+      if(index1 != NULL) free(index1);
+      index1 = NULL;
+      break;
+    }
+    index1 = index2;
+    i++;
+  } while(1);
+
+  if((index1 != NULL) && (i > 0)){
+    int index_size = i * sizeof(struct index_list);
+    index2 = malloc(index_size);
+    if(index2 != NULL) memcpy(index2, index1, index_size);
+    free(index1);
+    if(indexs != NULL) *indexs = index2;
+    return i;
+  }
+  if(index1 != NULL) free(index1);
+  if(indexs != NULL) *indexs = NULL;
+  return 0;
+}
+
+static AG_Tlist *GenIndex(AG_Widget *parent, char *buf, char **buf2, struct index_list **lines)
+{
+  char *p, *q, *pp;
+  int i;
+  AG_Tlist *tbl;
+  AG_TlistItem *item;
+  char *OneLineHead;
+  char *nodehead, *nodename, *nodepos;
+  long pos;
+  int linecnt;
+  struct index_list *indexs;
+  
+  const char *header = "\x1f\x0aTag Table:";
+  const char *footer = "End Tag Table";
+  if(buf == NULL) return NULL;
+  if(buf2 == NULL) return NULL;
+  p = strstr(buf, header);
+  q = strstr(p, footer);
+
+  if(p != NULL) {
+    int len;
+    len = p - buf;
+    pp = malloc((len + 32) * sizeof(char));
+    strncpy(pp, buf, len);
+    *buf2 = pp;
+    pp[len] = '\0';
+  } 
+  if((p == NULL) || (q == NULL)) {
+    return NULL;
+  }
+
+  /*
+   * Parse index as right WIDGET.
+   */
+  tbl = AG_TlistNew(parent, AG_TLIST_TREE | AG_TLIST_VFILL);
+  AG_TlistSizeHint(tbl, "xxxxxxxxxxxxxxxxxx", 25);
+  
+  linecnt = GetIndexLines(p, q, &indexs);
+  i = 0;
+    for(i = 0; i < linecnt; i++) {
+    if((indexs != NULL) && (i <= linecnt)) {
+      item = AG_TlistAddPtr(tbl, NULL, indexs[i].indexname, &indexs[i]);
+    }
+  }
+  if(lines != NULL) *lines = indexs;
+  return tbl;
 }
 
 static void OnHelpDialog(AG_Event *event)
@@ -141,8 +260,11 @@ static void OnHelpDialog(AG_Event *event)
   AG_Button *button;
   AG_Box *box;
   AG_Textbox *textbox;
+  AG_Tlist *index = NULL;
   SDL_RWops *fp;
-  Uint8 *buf;
+  Uint8 *buf = NULL;
+  Uint8 *buf2 = NULL;
+  struct index_list *indexs = NULL;
   int size = 0;
 
   win= AG_WindowNew(DIALOG_WINDOW_DEFAULT);
@@ -150,29 +272,33 @@ static void OnHelpDialog(AG_Event *event)
 
   fp = OpenInfoByLocale();
   buf = ReadInfoFile(fp, &size);
-//  textbox = AG_TextboxNew(box, AG_TEXTBOX_MULTILINE | AG_TEXTBOX_READONLY | AG_TEXTBOX_VFILL, "%p", buf);
-  if(buf == NULL) {
+  if(buf != NULL) {
+    index = GenIndex(AGWIDGET(box), buf, &buf2, &indexs);
+    free(buf);
+  }
+  
+  if(buf2 == NULL) {
     const char *defaultstr = "Error opening info file.";
-    buf = malloc(strlen(defaultstr) + 10);
-    strcpy(buf, defaultstr);
+    buf2 = malloc(strlen(defaultstr) + 10);
+    strcpy(buf2, defaultstr);
     size = strlen(defaultstr);
-    //AG_TextboxSetString(textbox, buf);
   }
 //  textbox = AG_TextboxNewS(box,  AG_TEXTBOX_MULTILINE | AG_TEXTBOX_CATCH_TAB | AG_TEXTBOX_EXPAND | AG_TEXTBOX_EXCL, NULL);
   {
      char hint[256];
-     memset(hint, "x", sizeof(char) * 200);
+     memset(hint, "x", sizeof(char) * 160);
      hint[200] = '\0';
      textbox = AG_TextboxNew(box,  AG_TEXTBOX_READONLY | AG_TEXTBOX_MULTILINE | AG_TEXTBOX_CATCH_TAB | AG_TEXTBOX_EXPAND, NULL);
-     AG_TextboxBindUTF8(textbox, buf, size);
+     AG_TextboxBindUTF8(textbox, buf2, size);
      AG_TextboxSizeHint(textbox, hint);
      AG_TextboxSizeHintLines(textbox, 25);
   }
-   
+  if(index != NULL) AG_TlistSetDblClickFn(index, OnClickNode, "%p,%p", index, textbox);
+
   {
     AG_Box *vbox;
     vbox = AG_BoxNewVert(AGWIDGET(box), AG_BOX_VFILL);
-    button = AG_ButtonNewFn(AGWIDGET(box), 0, gettext("OK"), OnCloseHelpViewer, "%p", buf);
+    button = AG_ButtonNewFn(AGWIDGET(box), 0, gettext("OK"), OnCloseHelpViewer, "%p%p", buf2, indexs);
   }
   AG_WindowSetCaption(win, gettext("HELP"));
   AG_WindowShow(win);
