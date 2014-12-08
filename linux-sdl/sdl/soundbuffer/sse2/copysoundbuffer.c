6895de5e883d4dfac30154e257ce3b4b0cc4b483
K.Ohta <whatisthis.sowhat@gmail.com>
2014-12-09 07:42:29 +0900

    [SND][SSE2] Using inline assembler, syntax is GAS/ATT, not MS/NASM/YASM.

--- linux-sdl/sdl/soundbuffer/sse2/copysoundbuffer.c
+++ linux-sdl/sdl/soundbuffer/sse2/copysoundbuffer.c
@@ -17,55 +17,73 @@ static inline Sint16 _clamp(Sint32 b)
     return (Sint16) b;
 }
 
-static inline v4hi _get_unaligned_int16(Sint16 *p)
+static v4hi _get_unaligned_int16(Sint16 *p)
 {
-   v4hi v;
+   v4hi v __attribute__((aligned(16)));
    
-   if(__builtin_expect((((uint64_t)p & 0x0f) == 0), 1)){
+//   if(__builtin_expect((((uint64_t)p & 0x0f) == 0), 1)){
       v = *((v4hi *)p);
-   } else {
-      v.ss[0] = *p++;
-      v.ss[1] = *p++;
-      v.ss[2] = *p++;
-      v.ss[3] = *p++;
-      v.ss[4] = *p++;
-      v.ss[5] = *p++;
-      v.ss[6] = *p++;
-      v.ss[7] = *p++;
-   }
+//   } else {
+//      v.ss[0] = *p++;
+//      v.ss[1] = *p++;
+//      v.ss[2] = *p++;
+//      v.ss[3] = *p++;
+//      v.ss[4] = *p++;
+//      v.ss[5] = *p++;
+//      v.ss[6] = *p++;
+//      v.ss[7] = *p++;
+//   }
    return v;
 }
 
-static inline v4hi _get_unaligned_int32(Sint32 *p)
+static v4hi _get_unaligned_int32(Sint32 *p)
 {
-   v4hi v;
-   
+   v4hi v __attribute__((aligned(16)));
+#if 0   
    if(__builtin_expect((((uint64_t)p & 0x0f) == 0), 1)){
       v = *((v4hi *)p);
    } else {
-      v.si[0] = *p++;
-      v.si[1] = *p++;
-      v.si[2] = *p++;
-      v.si[3] = *p++;
+      v.si[0] = p[0];
+      v.si[1] = p[1];
+      v.si[2] = p[2];
+      v.si[3] = p[3];
    }
+#else
+   v4hi *s = (v4hi *)p;
+   asm volatile("movdqu %[x], %%xmm0\n\t"
+		"movdqu %%xmm0, %[y]"
+		: [y] "=rm" (v)
+		: [x] "rm" (*s)
+		: "xmm0");
+   
+#endif
    return v;
 }
 
-static inline void  _put_unaligned_int16(Sint16 *dst, v4hi v)
+static void  _put_unaligned_int16(Sint16 *dst, v4hi v)
 {
-   
+#if 0   
    if(__builtin_expect((((uint64_t)dst & 0x0f) == 0), 1)){
       *((v4hi *)dst) = v;
    } else {
-      *dst++ = v.ss[0];
-      *dst++ = v.ss[1];
-      *dst++ = v.ss[2];
-      *dst++ = v.ss[3];
-      *dst++ = v.ss[4];
-      *dst++ = v.ss[5];
-      *dst++ = v.ss[6];
-      *dst++ = v.ss[7];
+      dst[0] = v.ss[0];
+      dst[1] = v.ss[1];
+      dst[2] = v.ss[2];
+      dst[3] = v.ss[3];
+      dst[4] = v.ss[4];
+      dst[5] = v.ss[5];
+      dst[6] = v.ss[6];
+      dst[7] = v.ss[7];
    }
+#else
+   v4hi *d = (v4hi *)dst;
+   asm volatile("movdqu %[y], %%xmm0\n\t"
+		"movdqu %%xmm0, %[x]"
+		: [x] "=rm" (*d)
+		: [y] "rm" (v)
+		: "xmm0"
+		);
+#endif
 }
 
 
@@ -87,18 +105,29 @@ static inline void  _put_unaligned_int16(Sint16 *dst, v4hi v)
 #if (__GNUC__ >= 4)
     v4hi t1, t2;
     v4hi tt;
+#if 0
     Sint32 *opn  = opnsrc;
     Sint16 *beep = beepsrc;
     Sint16 *cmt  = cmtsrc;
     Sint16 *wav  = wavsrc;
     Sint16 *p    = dst;
-    register v4hi vtmp;
+    v4hi vtmp;
+#else
+    v4hi *opn = (v4hi *)opnsrc;
+    v4hi *beep = (v4hi *)beepsrc;
+    v4hi *cmt = (v4hi *)cmtsrc;
+    v4hi *wav = (v4hi *)wavsrc;
+    v4hi *opnb = &opn[1];
+    v4hi *p    = (v4hi *)dst;
+
+#endif
    _prefetch_data_write_l1(p, samples * sizeof(Sint16));
    _prefetch_data_read_l2(opn, sizeof(Sint32) * samples);
    _prefetch_data_read_l2(beep, sizeof(Sint16) * samples);
    _prefetch_data_read_l2(cmt, sizeof(Sint16) * samples);
    _prefetch_data_read_l2(wav, sizeof(Sint16) * samples);
    for(i = 0; i < len1; i++) {
+#if 0
         t1 = _get_unaligned_int32(opn);
         t2 = _get_unaligned_int32(opn + 4);
         opn += 8;
@@ -110,10 +139,38 @@ static inline void  _put_unaligned_int16(Sint16 *dst, v4hi v)
         vtmp = _get_unaligned_int16(wav);
         tt.vv = __builtin_ia32_paddsw128(tt.vv, vtmp.vv);
         _put_unaligned_int16(p, tt);
+        opn += 8;
         beep += 8;
         cmt += 8;
         wav += 8;
         p += 8;
+
+#else
+      // Assembler syntax is ATT, not MS, NASM, YSAM.
+      asm volatile("movdqu %[opn1],  %%xmm1\n\t"
+		   "movdqu %[opn2],  %%xmm0\n\t"
+		   "packssdw %%xmm0, %%xmm1\n\t"
+		   "movdqu %[beep1], %%xmm0\n\t"
+		   "paddsw %%xmm0, %%xmm1\n\t"
+		   "movdqu %[cmt1],  %%xmm0\n\t"
+		   "paddsw %%xmm0, %%xmm1\n\t"
+		   "movdqu %[wav1],  %%xmm0\n\t"
+		   "paddsw %%xmm0, %%xmm1\n\t"
+		   "movdqu %%xmm1, %[dstp]"
+		   : [dstp] "=rm" (*p)
+		   : [opn1] "rm" (*opn),
+		     [opn2] "rm" (*opnb),
+		     [beep1] "rm" (*beep),
+		     [cmt1] "rm" (*cmt),
+		     [wav1] "rm" (*wav)
+		   : "xmm0", "xmm1");
+      p++;
+      opn  = &opn[2];
+      opnb = &opnb[2];
+      beep++;
+      cmt++;
+      wav++;
+#endif
    }
 #endif   
 //   if(len2 <= 0) return len1 * 8;
