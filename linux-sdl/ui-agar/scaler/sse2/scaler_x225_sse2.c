f5b712fbe534d9efab1667fb681bf33fe5795db2
K.Ohta <whatisthis.sowhat@gmail.com>
2014-12-11 01:46:01 +0900

    [Draw][Scaler][SDLFB][SSE2] Rewrite algorythm of x2.25.

--- linux-sdl/ui-agar/scaler/sse2/scaler_x225_sse2.c
+++ linux-sdl/ui-agar/scaler/sse2/scaler_x225_sse2.c
@@ -46,114 +46,9 @@ static void Scaler_DrawLine(Uint32 *dst, Uint32 *src, int ww, int repeat, int pi
 
    _prefetch_data_write_l1(r5v, sizeof(r5v));
    if((bFullScan) || (repeat < 2)) {
-	 // 76543210 -> 776655444332211000
-#if 0
-      for(xx = 0; xx < ww; xx += 8) {
-	 r1 = b[0];
-	 r2 = b[1];
-	 r3 = b[2];
-	 r4 = b[3];
-	 r5v[ip + 0].uv = (v2ui){r1.i[0], r1.i[0]}; //00
-	 r5v[ip + 1].uv = (v2ui){r1.i[0], r1.i[1]}; //01
-	 
-	 r5v[ip + 2].uv = (v2ui){r1.i[1], r2.i[0]}; //12
-	 r5v[ip + 3].uv = (v2ui){r2.i[0], r2.i[1]}; //23
-	 
-	 r5v[ip + 4].uv = (v2ui){r2.i[1], r3.i[0]}; //34
-	 r5v[ip + 5].uv = (v2ui){r3.i[0], r3.i[0]}; //44
-	 r5v[ip + 6].uv = (v2ui){r3.i[1], r3.i[1]}; //55
-
-	 r5v[ip + 7].uv = (v2ui){r4.i[0], r4.i[0]}; //66
-	 r5v[ip + 8].uv = (v2ui){r4.i[1], r4.i[1]}; //77	 
-	 ip += 9;
-	 b += 4;
-      }
-      b2p = (v2hi *)dst;      
-      _prefetch_data_read_l1(r5v, sizeof(r5v));
-      for(yy = 0; yy < repeat; yy++) {
-	 memcpy((void *)b2p, (void *)r5v, ip * sizeof(v2hi));
-	 b2p = b2p + pitch2;
-      }
-#else
-	 asm volatile (
-		   "movq %%rsp, %%rbp\n\t"
-		   "subq $32, %%rsp\n\t"
-		   "movl %[ww], %%edx\n\t"
-		   "shr  $3, %%edx\n\t"
-		   "movq %[src], %%rsi\n\t"
-		   "movq %[dst], %%rdi\n\t"
-		   "movl %[pitch], %%ebx\n\t"
-		   "_l1:\n\t"    
-		   "movq %%rdi, -16(%%rbp)\n\t"
-		   "movdqu 0(%%rsi), %%xmm0\n\t"
-		   "pshufd $0b11111010 ,%%xmm0, %%xmm1\n\t"
-		   "pshufd $0b01010000 ,%%xmm0, %%xmm2\n\t"
-		   "movd %%xmm0, %%eax\n\t"
-		   "movdqu 16(%%rsi), %%xmm0\n\t"
-                   "movdqa %%xmm0, %%xmm3\n\t"
-                   "movdqa %%xmm0, %%xmm4\n\t"
-		   "movd %%eax, %%xmm0\n\t"
-		   "pslldq $28, %%xmm0\n\t"
-		   "psrldq $4, %%xmm3\n\t"
-		   "por %%xmm3, %%xmm0\n\t"
-		   "pshufd $0b10101001, %%xmm0, %%xmm0\n\t"
-		   "pshufd $0b10100101, %%xmm4, %%xmm3\n\t"
-		   "movd %%xmm4, %%eax\n\t"
-		   "movl %[rep], %%ecx\n\t"
-		   "movq -16(%%rbp), %%rdi\n"
-		   "_l2:\n\t"
-		   "movdqu %%xmm2, 0(%%rdi)\n\t"
-		   "movdqu %%xmm1, 16(%%rdi)\n\t"
-		   "movdqu %%xmm3, 40(%%rdi)\n\t"
-		   "movl   %%eax,  32(%%rdi)\n\t"
-		   "movl   %%eax,  36(%%rdi)\n\t"
-		   "movdqu %%xmm0, 56(%%rdi)\n\t"
-		   "addq   %%rbx, %%rdi\n\t"
-		   "dec %%ecx\n\t"
-		   "jnz _l2\n\t"
-		   "addq $32, %%rsi\n\t"
-		   "movq -16(%%rbp), %%rdi\n\t"
-		   "addq $72, %%rdi\n\t"
-		   "dec %%edx\n\t"
-		   "jnz _l1\n\t"
-		   "movq %%rbp, %%rsp\n\t"
-		   :
-		   : [pitch] "rm"(pitch), [ww]"rm" (ww),
-		     [rep] "rm"(repeat),
-		     [src] "rm" (sp), [dst] "rm" (dp)
-		   : "eax","rbx","rdi", "rsi",  "ecx", "edx", "rbp", "xmm0","xmm1","xmm2","xmm3","xmm4");
-#endif      
+      yrep2 = repeat;
    } else {
-#if 0
       // 76543210 -> 776655444332211000
-      for(xx = 0; xx < ww; xx += 8) {
-	 r1 = b[0];
-	 r2 = b[1];
-	 r3 = b[2];
-	 r4 = b[3];
-	 r5v[ip + 0].uv = (v2ui){r1.i[0], r1.i[0]}; //00
-	 r5v[ip + 1].uv = (v2ui){r1.i[0], r1.i[1]}; //01
-	 
-	 r5v[ip + 2].uv = (v2ui){r1.i[1], r2.i[0]}; //12
-	 r5v[ip + 3].uv = (v2ui){r2.i[0], r2.i[1]}; //23
-	 
-	 r5v[ip + 4].uv = (v2ui){r2.i[1], r3.i[0]}; //34
-	 r5v[ip + 5].uv = (v2ui){r3.i[0], r3.i[0]}; //44
-	 r5v[ip + 6].uv = (v2ui){r3.i[1], r3.i[1]}; //55
-
-	 r5v[ip + 7].uv = (v2ui){r4.i[0], r4.i[0]}; //66
-	 r5v[ip + 8].uv = (v2ui){r4.i[1], r4.i[1]}; //77	 
-	 ip += 9;
-	 b += 4;
-      }
-      b2p = (v2hi *)dst;      
-      _prefetch_data_read_l1(r5v, sizeof(r5v));
-      for(yy = 0; yy < repeat - 1; yy++) {
-	 memcpy((void *)b2p, (void *)r5v, ip * sizeof(v2hi));
-	 b2p = b2p + pitch2;
-      }
-      for(xx = 0; xx < ip; xx++) b2p[xx].uv = bb;
-#else
       yrep2 = repeat - 1;
       if(yrep2 < 1) {
 	 yrep2 = 1;
@@ -161,6 +56,41 @@ static void Scaler_DrawLine(Uint32 *dst, Uint32 *src, int ww, int repeat, int pi
       } else {
 	 yrep3 = 1;
       }
+   }
+#ifndef __x86_64__ /* ia32 etc */
+	 // 76543210 -> 776655444332211000
+   _prefetch_data_write_l1(r5v, sizeof(r5v));
+   for(xx = 0; xx < ww; xx += 8) {
+      r1 = b[0];
+      r2 = b[1];
+      r3 = b[2];
+      r4 = b[3];
+      r5v[ip + 0].uv = (v2ui){r1.i[0], r1.i[0]}; //00
+      r5v[ip + 1].uv = (v2ui){r1.i[0], r1.i[1]}; //01
+      
+      r5v[ip + 2].uv = (v2ui){r1.i[1], r2.i[0]}; //12
+      r5v[ip + 3].uv = (v2ui){r2.i[0], r2.i[1]}; //23
+	 
+      r5v[ip + 4].uv = (v2ui){r2.i[1], r3.i[0]}; //34
+      r5v[ip + 5].uv = (v2ui){r3.i[0], r3.i[0]}; //44
+      r5v[ip + 6].uv = (v2ui){r3.i[1], r3.i[1]}; //55
+
+      r5v[ip + 7].uv = (v2ui){r4.i[0], r4.i[0]}; //66
+      r5v[ip + 8].uv = (v2ui){r4.i[1], r4.i[1]}; //77	 
+      ip += 9;
+      b += 4;
+   }
+   b2p = (v2hi *)dst;
+   //_prefetch_data_read_l1(r5v, sizeof(r5v));
+   for(yy = 0; yy < yrep2; yy++) {
+	 memcpy((void *)b2p, (void *)r5v, ww * sizeof(Uint32));
+	 b2p = b2p + pitch2;
+   }
+   for(yy = 0; yy < yrep3; yy++) {
+      for(xx = 0; xx < ip; xx++) b2p[xx].uv = bb;
+   }
+#else /* defined(__x86_64__) */
+   /* x86_64 : Using assembly. */
       asm volatile (
 		   "movq %%rsp, %%rbp\n\t"
 		   "subq $32, %%rsp\n\t"
@@ -222,8 +152,8 @@ static void Scaler_DrawLine(Uint32 *dst, Uint32 *src, int ww, int repeat, int pi
 		   : "eax","rbx","rdi", "rsi",  "ecx", "edx", "rbp", "xmm0","xmm1","xmm2","xmm3","xmm4");
       
 #endif      
-   }
 }
+
 
 
 
